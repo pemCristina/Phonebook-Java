4a746e4417cb88b28a4ba27e45da20795edecc57
Cristina Pérez <pem.cristina@gmail.com>
2017-01-29 18:46:52 +0100

    Some changes in Constants class

--- src/com/cristina/Constants.java
+++ src/com/cristina/Constants.java
@@ -2,6 +2,6 @@ package com.cristina;
 
 public class Constants {
     public static String CURRENT_DIRECTORY = System.getProperty("user.dir");
-    public static String FILE_NAME = "\\phonebook.txt";
-    public static String FILE_PATH = CURRENT_DIRECTORY + FILE_NAME;
+    public static String FILE_NAME = "phonebook.txt";
+    public static String FILE_PATH = CURRENT_DIRECTORY +"\\" + FILE_NAME;
 }
