a16a44dfacd7f88566e66a74f215a61ff95da694
Cristina Pérez <pem.cristina@gmail.com>
2017-01-29 14:32:35 +0100

    Tested main

--- src/com/cristina/Main.java
+++ src/com/cristina/Main.java
@@ -1,8 +1,49 @@
 package com.cristina;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+        Contacts contacts = new Contacts();
+        boolean end = false;
+
+        System.out.println("Welcome to Contacts!");
+        do {
+            System.out.print("(" + contacts.size() + ") > ");
+            String text = keyboard.nextLine();
+            OptionMenu option = OptionsParse.parse(text);
+            switch (option) {
+                case ADD:
+                    contacts.addContact();
+                    break;
+
+                case DELETE:
+                    contacts.deleteContact();
+                    break;
+
+                case LIST:
+                    break;
+
+                case HELP:
+                    displayHelp();
+                    break;
+
+                case QUIT:
+                    end = true;
+                    System.out.println("Bye!");
+            }
+        } while (!end);
+    }
+
+    private static void displayHelp() {
+        System.out.println("·········································");
+        System.out.println("·\tA - Add a new contact               ·");
+        System.out.println("·\tD - Delete a contact by position    ·");
+        System.out.println("·\tL - List a phonebook                ·");
+        System.out.println("·\tH - Display help                    ·");
+        System.out.println("·\tQ - Quit program                    ·");
+        System.out.println("·········································");
     }
 }
